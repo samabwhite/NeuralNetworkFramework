Corrected backpropagation methods in all classes and fully implemented network class back propagation. Also added a few more methods such as removeLayer() and updateWeights(). Backpropagation successfully decreased error over multiple passes! Further testing is needed.

NeuralNetworkFramework.py
@@ -8,8 +8,12 @@ class NeuralNetwork(object):
         self.epochs = epochs
         self.layers = [None] # first layer value is None as it represents the input layer
         self.errorLayer = None
+        self.complete = False
 
     def addHiddenLayer(self, inputSize, outputSize, layerNumber):   # add first layer or automatically checks compatibility and adds layer to end '
+        if layerNumber >= len(self.layers) and self.complete:
+            print("Can't add dense layers after error.")
+            return
         newLayer = HiddenLayer(inputSize, outputSize) 
         if not self.checkLayerCompatibility(newLayer, layerNumber):
             return
@@ -21,25 +25,44 @@ class NeuralNetwork(object):
     def addError(self): # figure out a way to prevent hidden layers to be added after error layer
         self.errorLayer = Error()
         self.layers.append(self.errorLayer)
+        self.complete = True
+
+    def removeLayer(self, layerNumber):
+        if layerNumber == 0:
+            print("Can't remove input layer from network.")
+            return
+        del self.complete[layerNumber]
 
     def forwardPass(self):
-        network = self.layers
-        network[1].forwardPass(self.dataset)
-        for index in range(2, len(network) - 1):
-            network[index].forwardPass(network[index-1].output)
-        network[-1].forwardPass(network[-2].output, self.labels)
+        layer = self.layers
+        layer[1].forwardPass(self.dataset)
+        layer[1].activationLayer.forwardPass(layer[1].output)
+        if len(layer) > 3:
+            for index in range(2, len(layer) - 1):
+                layer[index].forwardPass(layer[index-1].activationLayer.output)
+                layer[index].activationLayer.forwardPass(layer[index].output)
+        self.errorLayer.forwardPass(layer[-2].activationLayer.output, self.labels)
 
     def backwardPass(self):
-        pass
+        layer = self.layers
+        # call backward pass on error
+        errorDerivative = self.errorLayer.backwardPass()
+        # loop through each layer backward passing
+        for index in range(2, len(self.layers)):
+            layer[-index].backwardPass(layer, errorDerivative)
+        # update parameters
+
+    def updateWeights(self):
+        layer = self.layers
+        for index in range(2, len(self.layers)):
+            layer[-index].update(self.alpha)
 
     def displayNetwork(self):
         for layer in self.layers[1:]:
             print(str(layer.output))
 
-    def displayNetworkOutput(self):
+    def displayNetworkError(self):
         print("Error: " + str(self.errorLayer.totalError))
-
-
 
 
     def checkLayerCompatibility(self, newLayer, layerNumber):
@@ -74,6 +97,10 @@ class NeuralNetwork(object):
         # if layer is being placed at end, check front compatibility only
         elif layerNumber == len(self.layers):
             return checkFit(self.layers[-1], newLayer)
+        # if layer is being placed in an empty section of the array
+        elif layerNumber >= len(self.layers):
+            print("WARNING: Layer was added in empty section of network array!")
+            return True
         # if layer is being placed in the middle, check both sides compatibility
         else:
             if checkFit(self.layers[layerNumber-1], newLayer) and checkFit(newLayer, self.layers[layerNumber]):
@@ -120,25 +147,29 @@ class HiddenLayer(Layer):
         return self.output
 
 
-    def backwardPass(self, errorDeriv, softMaxDeriv, sigmoidDeriv):
+    def backwardPass(self, network, errorDerivative):
         def foil(dZ, input):
             dW = []
             for i in dZ:
                 for k in input:
                     dW.append(i * k)
             return dW
-        if self.nextLayer == None: 
-            self.dZ = errorDeriv * softMaxDeriv
+
+        # if this layer is the output layer, 
+        if network[-2] == self:
+            self.dZ = errorDerivative * self.activationLayer.backwardPass()
             self.dB = self.dZ
-            self.dW = np.array(foil(self.dZ, self.prevLayer.output))
-        else:   
-            self.dZ = np.dot(self.nextLayer.dZ, self.nextLayer.weights) * sigmoidDeriv
+            self.dW = np.array(foil(self.dZ, network[-3].output))
+        else:
+            layerIndex = network.index(self)
+            self.dZ = np.dot(network[layerIndex+1].dZ, network[layerIndex+1].weights) * self.activationLayer.backwardPass()
             self.dB = self.dZ
             self.dW = np.array(foil(self.dZ, self.input))
+        
 
-    def update(self):
-        self.weights += (-self.alpha * np.reshape(self.dW, (2,2)))
-        self.bias += (-self.alpha * self.dB)
+    def update(self, alpha):
+        self.weights += (-alpha * np.reshape(self.dW, ((self.dW.shape[0]//2),2)))
+        self.bias += (-alpha * self.dB)
 
 
     def setActivation(self, type):
@@ -162,6 +193,7 @@ class SigmoidActivation(Layer):
 
     def backwardPass(self):
         self.derivative = self.output * (np.subtract(1, self.output))
+        return self.derivative  
 
     def update(self):
         pass
@@ -182,6 +214,7 @@ class SoftMaxActivation(Layer):
 
     def backwardPass(self):
         self.derivative = self.output * (np.subtract(1, self.output)) # softMaxOutput * (1 - softMaxOutput)
+        return self.derivative 
 
     def update(self):
         pass
@@ -203,6 +236,7 @@ class Error(Layer):
 
     def backwardPass(self):
         self.derivative = np.subtract(self.networkOutput, self.labels) # output - labels 
+        return self.derivative
 
     def totalSquaredError(self, predicted, labels):
         return np.sum((1/2) * np.power(np.subtract(labels, predicted), 2))
@@ -228,6 +262,15 @@ nn.addActivation(2, "Softmax")
 nn.addError()
 
 nn.forwardPass()
+nn.displayNetworkError()
+nn.backwardPass()
+nn.updateWeights()
+nn.forwardPass()
+nn.displayNetworkError()
+nn.backwardPass()
+nn.updateWeights()
+
+
 
 
 