Forward and backward propagation bug fixes. Was able to get an average of roughly 50% error. This is telling me that the network is averaging out it's guesses and it's not actually guessing based on the information given. Lots of fixes ahead.

NeuralNetworkFramework.py
@@ -58,8 +58,8 @@ class NeuralNetwork(object):
             layer[-index].update(self.alpha)
 
     def displayNetwork(self):
-        for layer in self.layers[1:]:
-            print(str(layer.output))
+        for layer in self.layers[1:-1]:
+            print(str(layer.weights))
 
     def displayNetworkError(self):
         print("Error: " + str(self.errorLayer.totalError))
@@ -113,6 +113,10 @@ class NeuralNetwork(object):
             self.displayNetworkError()
             self.backwardPass()
             self.updateWeights()
+            print(self.errorLayer.networkOutput)
+            print(str(label) + "\n")
+            
+        
         
 
 
@@ -148,7 +152,7 @@ class HiddenLayer(Layer):
     
     def forwardPass(self, input):
         self.input = input
-        self.output = np.dot(self.input, self.weights) + self.bias.T
+        self.output = np.dot(input, self.weights) + self.bias.T
         return self.output
 
 
@@ -173,8 +177,8 @@ class HiddenLayer(Layer):
         
 
     def update(self, alpha):
-        self.weights += (-alpha * np.reshape(self.dW, (self.weights.shape)))
-        self.bias += (-alpha * self.dB)
+        self.weights += (alpha * np.reshape(self.dW, (self.weights.shape)))
+        self.bias += (alpha * self.dB)
 
 
     def setActivation(self, type):
@@ -192,7 +196,7 @@ class SigmoidActivation(Layer):
         pass
 
     def forwardPass(self, input):
-        self.input = input
+        self.input = input 
         self.output = 1 / (1 + np.exp(-self.input))
         return self.output
 
@@ -213,8 +217,12 @@ class SoftMaxActivation(Layer):
     def forwardPass(self, input):
         self.input = input
         self.output = []
+        self.expValues = self.input - np.max(self.input) # axis=1, keepdims=True
+        self.output = self.expValues / np.sum(self.expValues) # axis=1, keepdims=True
+        '''
         for i in input:
             self.output.append(np.exp(i) / np.sum(np.exp(input)))
+            '''
         return self.output
 
     def backwardPass(self):
@@ -240,7 +248,7 @@ class Error(Layer):
         self.output = self.totalError
 
     def backwardPass(self):
-        self.derivative = np.subtract(self.networkOutput, self.labels) # output - labels 
+        self.derivative = np.subtract(self.networkOutput, self.labels) # output - labels # HERE THIS IS THE SAME AS SQUARED ERROR DERIVATIVE BUT IS PRODUCING DIFFERENT VALUES
         return self.derivative
 
 

sandbox.py
@@ -1,20 +1,50 @@
 import NeuralNetworkFramework as nnf
 import numpy as np
+import pandas as pd
 
-dataset = np.array([[1, 2, 3, 4], [4, 3, 2, 1], [2, 3, 4, 5], [6, 5, 4, 3], [1, 3, 5, 1]])
-oneHotLabels = np.array([[0, 1], [0, 1], [1, 0], [0, 1], [0, 1]])
+'''
+dataset = np.array([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]])
+oneHotLabels = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
 
-network = nnf.NeuralNetwork(dataset, oneHotLabels, 5, 0.1)
-network.addHiddenLayer(4, 3, 1)
+network = nnf.NeuralNetwork(dataset, oneHotLabels, len(dataset) - 1, 1)
+network.addHiddenLayer(6, 5, 1)
 network.addActivation(1, "Sigmoid")
-network.addHiddenLayer(3, 3, 2)
+network.addHiddenLayer(5, 4, 2)
 network.addActivation(2, "Sigmoid")
-network.addHiddenLayer(3, 2, 3)
+network.addHiddenLayer(4, 3, 3)
 network.addActivation(3, "Softmax")
 network.addError()
 
 network.train() 
 
+'''
+
+
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+
+data = pd.read_csv("C:\\Users\\samwh\\Desktop\\Neural Network\\train.csv")
+data = np.array(data)
+data = data[0:1000].T
+Y = one_hot(data[0]).T
+X = data[1:].T 
+
+
+
+network = nnf.NeuralNetwork(X, Y, 1000, 0.01)
+
+network.addHiddenLayer(784, 10, 1)
+network.addActivation(1, "Sigmoid")
+network.addHiddenLayer(10, 10, 2)
+network.addActivation(2, "Softmax")
+network.addError()
+
+network.train()
 
 
 