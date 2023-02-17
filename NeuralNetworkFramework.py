Bug fixes to test MNIST dataset. Results are producing completely random guesses and averages slightly above a 10% accuracy. Next is to rework the backpropagation algorithm to confirm that it is calculating correctly.

NeuralNetworkFramework.py
@@ -10,6 +10,7 @@ class NeuralNetwork(object):
         self.errorLayer = None
         self.complete = False
 
+
     def addHiddenLayer(self, inputSize, outputSize, layerNumber):   # add first layer or automatically checks compatibility and adds layer to end '
         if layerNumber >= len(self.layers) and self.complete:
             print("Can't add dense layers after error.")
@@ -108,14 +109,20 @@ class NeuralNetwork(object):
             return False
 
     def train(self):
-        for data, label in zip(self.dataset, self.labels):
-            self.forwardPass(data, label)
-            self.displayNetwork()
+        if not self.complete:
+            print("Error Layer Missing: Network not complete")
 
-            #self.backwardPass()
-            #self.updateWeights()
-            #print(self.errorLayer.networkOutput)
-            #print(str(label) + "\n")
+        for data, label in zip(self.dataset, self.labels):
+    
+            self.forwardPass(data, label)
+            self.displayNetworkError()
+            self.backwardPass()
+            self.updateWeights()
+
+            
+            
+         
+            
             
         
         
@@ -178,8 +185,8 @@ class HiddenLayer(Layer):
         
 
     def update(self, alpha):
-        self.weights += (alpha * np.reshape(self.dW, (self.weights.shape)))
-        self.bias += (alpha * self.dB)
+        self.weights += -(alpha * np.reshape(self.dW, (self.weights.shape)))
+        self.bias += -(alpha * self.dB)
 
 
     def setActivation(self, type):
@@ -202,6 +209,7 @@ class SigmoidActivation(Layer):
         return self.output
 
     def backwardPass(self):
+        #self.derivative = (np.exp(-self.output))/((np.exp(-self.output)+1)**2)
         self.derivative = self.output * (np.subtract(1, self.output))
         return self.derivative  
 
@@ -238,7 +246,8 @@ class SoftMaxActivation(Layer):
 
 class Error(Layer):
     def __init__(self):
-        pass
+        self.correct = 0
+        self.incorrect = 0
 
     def forwardPass(self, networkOutput, labels):
         self.networkOutput = networkOutput
@@ -255,10 +264,19 @@ class Error(Layer):
 
 
     def totalSquaredError(self, predicted, labels):
+        print(labels)
+        print(np.round(predicted)) 
+        if np.argmax(self.networkOutput) == np.where(labels == 1):
+                self.correct += 1
+        else:
+                self.incorrect += 1
+        print(self.correct / (self.correct + self.incorrect))
+
         return np.sum((1/2) * np.power(np.subtract(labels, predicted), 2))
 
     def squaredErrorDerivative(self, predicted, labels):
-        return -np.subtract(labels, predicted)
+        return np.subtract(predicted, labels)
+
 
 
 

sandbox.py
@@ -2,9 +2,9 @@ import NeuralNetworkFramework as nnf
 import numpy as np
 import pandas as pd
 
-
-dataset = np.array([1, 2, 3, 4])
-labels = np.array([0.01, 0.99])
+'''
+dataset = np.array([[1, 2, 3, 4], [1, 2, 3, 4]])
+labels = np.array([[0.01, 0.99], [0.01, 0.99]])
 
 
 nn = nnf.NeuralNetwork(dataset, labels, 1, 0.1)
@@ -23,6 +23,9 @@ nn.layers[2].weights = np.array([[0.65, 0.80], [0.70, 0.85], [0.75, 0.90]])
 nn.layers[2].bias = np.array([0.25, 0.5])
 
 
+
+
+
 layer1Output = np.dot(dataset, nn.layers[1].weights)
 print(layer1Output)
 layer1Activation = 1 / (1 + np.exp(-layer1Output))
@@ -35,52 +38,8 @@ layer2Activation = np.exp(layer2Adjust) / np.sum(np.exp(layer2Adjust))
 print(layer2Activation)
 error = labels - layer2Activation
 print(error)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-dataset = np.array([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]])
-oneHotLabels = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
-
-network = nnf.NeuralNetwork(dataset, oneHotLabels, len(dataset) - 1, 1)
-network.addHiddenLayer(6, 5, 1)
-network.addActivation(1, "Sigmoid")
-network.addHiddenLayer(5, 4, 2)
-network.addActivation(2, "Sigmoid")
-network.addHiddenLayer(4, 3, 3)
-network.addActivation(3, "Softmax")
-network.addError()
-
-network.train() 
-
 '''
 
-
-'''
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
@@ -90,13 +49,13 @@ def one_hot(Y):
 
 data = pd.read_csv("C:\\Users\\samwh\\Desktop\\Neural Network\\train.csv")
 data = np.array(data)
-data = data[0:1000].T
+data = data[0:].T
 Y = one_hot(data[0]).T
-X = data[1:].T 
+X = data[1:].T
 
 
 
-network = nnf.NeuralNetwork(X, Y, 1000, 0.01)
+network = nnf.NeuralNetwork(X, Y, len(X), 0.01)
 
 network.addHiddenLayer(784, 10, 1)
 network.addActivation(1, "Sigmoid")
@@ -105,7 +64,5 @@ network.addActivation(2, "Softmax")
 network.addError()
 
 network.train()
-'''
-
 
 